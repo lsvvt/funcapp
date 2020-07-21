bbc5fefb24a9064f325e9722f36145cc4b7e3aa2
lsvvt <losev.tim.v@gmail.com>
2020-07-21 15:33:59 +0300

    Up

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from inf import app, server
-from tabs import intro, predict, explain, evaluate
+from tabs import info
 
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
 app.layout = html.Div([
     dcc.Markdown('Title'),
-    dcc.Tabs(id='tabs', value='tab-intro', children=[
-        dcc.Tab(label='Info', value='tab-intro'),
+    dcc.Tabs(id='tabs', value='tab-info', children=[
+        dcc.Tab(label='Info', value='tab-info'),
         # dcc.Tab(label='RHO PBE0', value='tab-1'),
         # dcc.Tab(label='DB PBE0', value='tab-2'),
         # dcc.Tab(label='DB Others', value='tab-3'),
@@ -21,7 +21,7 @@ app.layout = html.Div([
 @app.callback(Output('tabs-content', 'children'),
               [Input('tabs', 'value')])
 def render_content(tab):
-    if tab == 'tab-intro': return intro.layout
+    if tab == 'tab-info': return info.layout
     # elif tab == 'tab-1': return 1.layout
     # elif tab == 'tab-2': return 2.layout
     # elif tab == 'tab-3': return 3.layout
--- tabs/info.py
+++ tabs/info.py
@@ -2,7 +2,7 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-from app import app
+from inf import app
 
 layout = [dcc.Markdown("""
 ### Info
